6b3b245cda66f4c1129f8f3b026b9d292fd1de4c
tomciv9000 <eugene.white@gmail.com>
2020-05-20 23:35:27 -0400

    remove requireAuth from index.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,16 @@ import React, { Component} from 'react';
 import {
   BrowserRouter,
   Switch,
-  Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation
+  Route
 } from "react-router-dom";
 import './App.css';
 
 import PrivatePage from './components/Private'
 import {connect} from 'react-redux';
-import { useDispatch, useSelector } from 'react-redux'
-import {getProfileFetch, logoutUser} from './actions/actions';
+import {getProfileFetch} from './actions/actions';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import NavigationBar from './components/NavigationBar';
-import requireAuth from './components/requireAuth';
 import { LandingPage } from './components/Landing';
 import AuthRoute from './components/AuthRoute'
 //i think it's possible to recreate the react tutotrial protexted routes
