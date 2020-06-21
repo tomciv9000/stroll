05d5d67c95127029b1334c8d81714e4c189828fc
tomciv9000 <eugene.white@gmail.com>
2020-06-21 10:59:26 -0700

    NavBar typo erros, spacing

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -9,15 +9,13 @@ class NavigationBar extends Component {
 
   handleClick = event => {
     event.preventDefault()
-    // Remove the token from localStorage
     localStorage.removeItem("token")
-    // Remove the user object from the Redux store
     this.props.logoutUser()
   }
 
   callUser = () => {
     if (this.props.current_user){
-    return (<p>User: {this.props.current_user.email}</p>)
+      return (<p>User: {this.props.current_user.email}</p>)
     }else {
       return (<p>Guest</p>)
     }
@@ -25,43 +23,35 @@ class NavigationBar extends Component {
 
   render() {
     const userLinks = (
-   
-    //<p>Welcome, {this.props.current_user.email}</p>
-      
-      
       <Nav className="mr-auto">
-        
         <Nav.Link href = "/" onClick={this.handleClick}>Log Out</Nav.Link>
-        
       </Nav>
-    );
+    )
 
     const guestLinks = (
       <Nav className="mr-auto">
         <Nav.Link href="/signup">Sign Up</Nav.Link>
         <Nav.Link href="/login">Log in</Nav.Link>
       </Nav>
-    );
+    )
 
     return (
-        //<nav className="navbar navbar-default">
-        <Navbar  bg= "dark" variant="dark" expand="lg">
-          
-          <Navbar.Brand href="/">
-            <img
-              alt=""
-              src={cropLogo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            Stroll
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            {this.props.auth ? userLinks : guestLinks}
-          </Navbar.Collapse>
-        </Navbar>
+      <Navbar  bg= "dark" variant="dark" expand="lg">
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src={cropLogo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Stroll
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {this.props.auth ? userLinks : guestLinks}
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
@@ -74,8 +64,7 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    //getProfileFetch: () => dispatch(getProfileFetch()),
     logoutUser: () => dispatch(logoutUser())
-  })
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar);
