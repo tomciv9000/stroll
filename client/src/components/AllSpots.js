e67518bf54b6e321f6186713ed4d6b7f5686ceab
tomciv9000 <eugene.white@gmail.com>
2020-06-21 10:55:25 -0700

    Clean Homepage

--- client/src/components/AllSpots.js
+++ client/src/components/AllSpots.js
@@ -2,22 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const AllSpots = (props) => {
-    //const spot = useSelector(state => state.places.spot)
     let spotData = props.spotData
     
-    
-
-        return (
-            <div>
-                <ul>
-                    <li>
-                    <Link 
-                        to={`/spots/${spotData.id}`}
-                        style={{textDecoration : 'none'}}
-                        className='button two place'>
-                        {spotData.location}</Link>
-                    </li>
-                </ul>
-            </div>
-        )
+    return (
+        <div>
+            <ul>
+                <li>
+                <Link 
+                    to={`/spots/${spotData.id}`}
+                    style={{textDecoration : 'none'}}
+                    className='button two place'>
+                    {spotData.location}</Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -13,74 +13,55 @@ class Homepage extends Component {
  
   componentDidMount = () => {
     this.props.getPlacesFetch()
-    console.log("Place Cleared")
     this.props.clearPlaceState()
   }
-
 
   callPlaceContainer = () => {
     if (!!this.props.places.length){
       return (<PlaceContainer places={this.props.places} />)
     } else {
-      return (<h5 className = "white-text">
-        Think of a place that's important to you.
-        </h5>)
+      return (<h5 className = "white-text">Think of a place that's important to you.</h5>)
     }
   }
 
   passToMap = () => {
-   if (this.props.places){
-    
-    return(
+    if (this.props.places){
+      return(
         <AllSpotsMap places={this.props.places} />
       )
-   } else {
-     return (<h1>No Places Registered Yet</h1>)
-   }
+    } else {
+      return (<h1>No Places Registered Yet</h1>)
+    }
   }
   
   render() {
     return (
       <div>
-      <Container >
-        <Row xs = {1} md= {2} lg={2} >
-        <Col lg xl={6} className = "question">
-          <h1 className = "question-text white-text">
-            Where have you been?
-          </h1>
-        </Col>
-         
-        <Col lg xl={6} className="answer">
-            <PlaceForm />
-        </Col>
-        </Row>
-        
-        <Row >
-          <Col  xs={3}className="places-list">
-            {this.callPlaceContainer()}
-          </Col>
-          <Col   className="map-box">
-            
-          
-       {this.passToMap()}
-       
-     
-            
-          </Col>
-     
-   
-        </Row>
-       
-        
-      </Container>
-     
-      
-      
-      
-        
-      
-        
-      
+        <Container >
+
+          <Row xs = {1} md= {2} lg={2} >
+            <Col lg xl={6} className = "question">
+              <h1 className = "question-text white-text">
+                Where have you been?
+              </h1>
+            </Col>
+
+            <Col lg xl={6} className="answer">
+                <PlaceForm />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col  xs={3}className="places-list">
+              {this.callPlaceContainer()}
+            </Col>
+
+            <Col   className="map-box">
+              {this.passToMap()}
+            </Col>
+          </Row>
+
+        </Container>
       </div>
     );
   }
