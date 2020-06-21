c280a51a5bfa8e0f26b02cb91814b2db57d15616
tomciv9000 <eugene.white@gmail.com>
2020-06-21 11:18:52 -0700

    Remove whitespace

--- client/src/components/SpotDetails.js
+++ client/src/components/SpotDetails.js
@@ -1,46 +1,26 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-//import { connect } from 'react-redux'; 
+import { useSelector } from 'react-redux'
 import AllMemories from './AllMemories'
 import CardDeck from 'react-bootstrap/CardDeck'
-import Row from 'react-bootstrap/Row'
-import { spotDeleteFetch } from '../actions/placeActions';
-
 
 export const SpotDetails = () => {
 
-
   const spot = useSelector(state => state.places.spot)
 
-
-
-  
   const call = () => {
-    
     if (spot.memories && spot.memories.length > 0){
-      
       return spot.memories.map((memoryItem) =>{
         return <AllMemories key = {memoryItem.id} memoryData={memoryItem} />
       })
-    }else {
-      
+    } else {
       return (<h5>No Memories Yet</h5>)
-
     }
   }
 
   return (
-    
-        
-        <CardDeck>
-          {call()}
-        </CardDeck>
-        
-        
-        
-         
-       
-   
+    <CardDeck>
+      {call()}
+    </CardDeck>
   )
 }
 
--- client/src/components/SpotShow.js
+++ client/src/components/SpotShow.js
@@ -8,117 +8,98 @@ import { getSpotFetch, clearSpotState, spotDeleteFetch } from '../actions/placeA
 import { SpotDetails } from './SpotDetails'
 import { Link } from 'react-router-dom';
 import SingleSpotMap from './map/SingleSpotMap'
-
 import '../index.css';
-
 
 class SpotShow extends Component{
 
-    componentDidMount(){ 
-      const {id} =  this.props.match.params;
-      this.props.getSpotFetch(id);   
-    }
+  componentDidMount(){ 
+    const {id} =  this.props.match.params;
+    this.props.getSpotFetch(id);   
+  }
 
-    componentDidUpdate(prevProps){
-      if (this.props.memory !== prevProps.memory){
-        console.log("A Memory was entered")
-        this.props.getSpotFetch(this.props.spot.id)
-        //this.props.spots.push(this.props.spot)
-      }
+  componentDidUpdate(prevProps){
+    if (this.props.memory !== prevProps.memory){
+      this.props.getSpotFetch(this.props.spot.id)
     }
+  }
 
-    componentWillUnmount(){
-      console.log("Unmounted Spot:Show ", this.props.spot)
-      this.props.clearSpotState()
-    }
+  componentWillUnmount(){
+    console.log("Unmounted Spot:Show ", this.props.spot)
+    this.props.clearSpotState()
+  }
 
-  //THIS IS UPDATED REGULARLY, THIS IS THE SWEET SPOT
-    callSpotDetails = () => {
-      
-      return (<SpotDetails />)
-      //if (this.props.place){
-      //  return (<PlaceDetails place = {this.props.place}/>)
-      //}else {
-      //  return (<h1>No info yet </h1>)
-      //}
-    }
-//
-    passPlaceInfo = () => {
-      if (this.props.spot.id){
-        let spot = this.props.spot
-        return (
-          <div><h1 className = "question-text white-text">{spot.location}</h1>
-          <Link  to = {`/places/${this.props.place.id}`} className="yellow-link" onClick={this.onDeleteClick}><small>[Delete {spot.location}]</small></Link>
-          
-           
-        <SingleSpotMap location={this.props.spot}/></div>
-          )
-        //return (<TestMap place={this.props.place} id={this.props.place.id} />)
-      }else {
-        return (<h1>No info yet </h1>)
-      }
-    }
+  callSpotDetails = () => {
+    return (<SpotDetails />)
+  }
 
-    linkBack = () => {
-        if(this.props.place){
-          return (
-          <div>
-            <p><Link className = "yellow-link" to={`/places/${this.props.place.id}`}>Back to Place Page</Link></p>
-         
-          </div>)
-        }else {
-          return(<p className = "white-text">loading...</p>)
-        }
+  passPlaceInfo = () => {
+    if (this.props.spot.id){
+      let spot = this.props.spot
+      return (
+        <div>
+          <h1 className = "question-text white-text">{spot.location}</h1>
+          <Link  to = {`/places/${this.props.place.id}`} className="yellow-link" onClick={this.onDeleteClick}>
+            <small>[Delete {spot.location}]</small>
+          </Link>
+          <SingleSpotMap location={this.props.spot}/>
+        </div>
+      )
+    } else {
+      return (<h1>No info yet </h1>)
     }
+  }
 
-    onDeleteClick = () => {
-      const {id} =  this.props.match.params;
-      this.props.spotDeleteFetch(id, () =>{
-        this.props.history.push(`/places/${this.props.place.id}`)
-      })
+  linkBack = () => {
+    if(this.props.place){
+      return (
+        <div>
+          <p><Link className = "yellow-link" to={`/places/${this.props.place.id}`}>Back to Place Page</Link></p>
+        </div>)
+    } else {
+      return(<p className = "white-text">loading...</p>)
     }
+  }
 
-    render(){
-      return(
-        <div >
-          {this.linkBack()}
+  onDeleteClick = () => {
+    const {id} =  this.props.match.params;
+    this.props.spotDeleteFetch(id, () =>{
+      this.props.history.push(`/places/${this.props.place.id}`)
+    })
+  }
+
+  render(){
+    return(
+      <div >
+        {this.linkBack()}
         <Container className = "spot-show">
           <Row>
             <Col>{this.passPlaceInfo()}</Col>
-           
-            
-            
           </Row>
-          <Row>
-          <Col><MemoryForm /></Col>  
-          
-            
-          </Row>
-          <Row>{this.callSpotDetails()}</Row>
-           
-          
-          
-        </Container>
-        
-        
-        </div>
 
-      )
-    }
+          <Row>
+            <Col><MemoryForm /></Col>  
+          </Row>
+
+          <Row>{this.callSpotDetails()}</Row>
+        </Container>
+      </div>
+    )
+  }
+
 }
 
 const mapStateToProps = (state) => {
-    return {
-      place: state.places.place,
-      spot: state.places.spot,
-      memory: state.places.memory
-    };
+  return {
+    place: state.places.place,
+    spot: state.places.spot,
+    memory: state.places.memory
+  };
 }
 
 const mapDispatchToProps = dispatch => ({
-    getSpotFetch: spotID => dispatch(getSpotFetch(spotID)),
-    clearSpotState: () => dispatch(clearSpotState()),
-    spotDeleteFetch: (spotID) => dispatch(spotDeleteFetch(spotID))
+  getSpotFetch: spotID => dispatch(getSpotFetch(spotID)),
+  clearSpotState: () => dispatch(clearSpotState()),
+  spotDeleteFetch: (spotID) => dispatch(spotDeleteFetch(spotID))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SpotShow)
