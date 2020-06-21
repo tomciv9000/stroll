3a45f2d032aeed098bf0b80780bca0f0e251fd51
tomciv9000 <eugene.white@gmail.com>
2020-06-21 11:34:10 -0700

    Remove console.logs from reducer

--- client/src/reducers/placesReducer.js
+++ client/src/reducers/placesReducer.js
@@ -11,79 +11,77 @@ const initialState = {
 export default function(state = initialState, action){
   switch(action.type){
     case FETCH_PLACES:
-  
       return{
         ...state,
         places: action.payload
       }
-      case NEW_PLACE:
+    
+    case NEW_PLACE:
       return {
         ...state,
         places: state.places.concat(action.payload),
         place: action.payload
       }
 
-      case GET_PLACE:
-
+    case GET_PLACE:
       return{
         ...state,
         place: action.payload
       }
-      case CLEAR_PLACE:
+      
+    case CLEAR_PLACE:
 
       return{
         ...state,
         place: {}
       }
 
-      case NEW_SPOT:
-        return{
-          ...state,
-          spot: action.payload
-        }
+    case NEW_SPOT:
+      return{
+        ...state,
+        spot: action.payload
+      }
 
-      case GET_SPOT:
-        return{
-          ...state,
-          spot: action.payload
-        }
-        case CLEAR_SPOT:
-  
-        return{
-          ...state,
-          spot: {}
-        }
-  
-          case NEW_MEMORY:
-            return{
-              ...state,
-              memory: action.payload
-            }
-          case DELETE_MEMORY:
-            console.log(state.first)
-            return{
-              ...state,
-              memory: {}
-            }
+    case GET_SPOT:
+      return{
+        ...state,
+        spot: action.payload
+      }
 
-            case DELETE_SPOT:
-            console.log(state.first)
-            return{
-              ...state,
-              spot: {}
-            }
+    case CLEAR_SPOT:
+      return{
+        ...state,
+        spot: {}
+      }
+  
+    case NEW_MEMORY:
+      return{
+        ...state,
+        memory: action.payload
+      }
 
-            case DELETE_PLACE:
-            console.log(state.first)
-            return{
-              ...state,
-              place: {status: "removed"}
-            }
+    case DELETE_MEMORY:
+      return{
+        ...state,
+        memory: {}
+      }
+
+    case DELETE_SPOT:
+      return{
+        ...state,
+        spot: {}
+      }
+
+    case DELETE_PLACE:
+      return{
+        ...state,
+          place: {status: "removed"}
+      }
   
-      default:
-        return state;
+    default:
+      return state;
   
-    }
   }
+}
 
 
