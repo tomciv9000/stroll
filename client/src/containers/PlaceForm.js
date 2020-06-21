dd3bc19838db463e1508b096e2df8339b806c16b
tomciv9000 <eugene.white@gmail.com>
2020-06-21 11:30:47 -0700

    Clean PlaceForm

--- client/src/containers/PlaceForm.js
+++ client/src/containers/PlaceForm.js
@@ -1,25 +1,13 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { placePostFetch, getPlacesFetch } from '../actions/placeActions'
 import { LoadScript, Autocomplete } from '@react-google-maps/api'
-//import PropTypes from 'prop-types';
 import '../index.css';
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 
 const libraries = ["places"]
-
-//function ValidationMessage(props) {
-//  if (!props.valid) {
-//    return(
-//      <div className='error-msg'>{props.message}</div>
-//    )
-//  }
-//  return null;
-//}
 
 class PlaceForm extends Component {
   constructor (props) {
@@ -35,38 +23,16 @@ class PlaceForm extends Component {
       user_id: this.props.user_id
     }
 }
-  
-//validateForm = () => {
-//  const { nameValid } = this.state;
-//  this.setState({
-//    formValid: nameValid
-//  })
-//}
-
-// validateName = () => {
-//   console.log("ValidateName exec")
-//   const { name } = this.state;
-//   let nameValid = true
-//   let errorMsg = {...this.state.errorMsg}
-
-//   if (!name || name.length < 1) {
-//     nameValid = false;
-//     errorMsg.name = "Cannot be empty"
-//   }
-//   console.log(nameValid)
-
-//   this.setState({nameValid, errorMsg}, this.validateForm)
-// }
-
-checkEmpty = () => {
-  let nameEmpty = false
-  if (!this.state.name || this.state.name.length < 0){
-    nameEmpty=true
-  } else {
-    nameEmpty=false
+ 
+  checkEmpty = () => {
+    let nameEmpty = false
+    if (!this.state.name || this.state.name.length < 0){
+      nameEmpty=true
+    } else {
+      nameEmpty=false
+    }
+    return nameEmpty
   }
-  return nameEmpty
-}
   
   handleChange = event => {
     this.setState({
@@ -80,19 +46,12 @@ checkEmpty = () => {
       this.googleField.current.value = ""
       this.setState({
             name: "",
-            
             lat: 0,
             lng: 0
           })
   }
-    
-  
 
-
-  
   onLoad (autocomplete) {
-    
-    //console.log('autocomplete: ', autocomplete)
     this.autocomplete = autocomplete
   }
 
@@ -102,7 +61,6 @@ checkEmpty = () => {
       if (!place.geometry) return;
       if (place.geometry.viewport){
           console.log("location", `${place.geometry.location.lat()} ${place.geometry.location.lng()}`)
-
           this.setState({  name: place.name, lat: place.geometry.location.lat(), lng: place.geometry.location.lng(), })
       }
     } else {
@@ -110,44 +68,34 @@ checkEmpty = () => {
     }
   }
 
-render() {
+  render() {
     return (
-        <div>
-
+      <div>
         <LoadScript
           googleMapsApiKey={(process.env.REACT_APP_GOOGLE_API_KEY)}
           libraries={libraries} 
         >
-        <Autocomplete
-          onLoad={this.onLoad}
-          onPlaceChanged={this.onPlaceChanged}
-        >
-          <InputGroup className="mb-3">
-          
+          <Autocomplete
+            onLoad={this.onLoad}
+            onPlaceChanged={this.onPlaceChanged}
+          >
+            <InputGroup className="mb-3">
                 <Form.Control
                   type="text"
                   ref={this.googleField}
                   name="city"
                   placeholder="Enter a city or region" 
                 />
-               
-              <InputGroup.Append>
-                <Button variant="outline-warning" disabled = {this.checkEmpty()} onClick={this.handleSubmit}>Add Place</Button>
-              </InputGroup.Append>
-              </InputGroup>
-              
-            
-          
-          
-        </Autocomplete>
-          
+                <InputGroup.Append>
+                  <Button variant="outline-warning" disabled = {this.checkEmpty()} onClick={this.handleSubmit}>Add Place</Button>
+                </InputGroup.Append>
+            </InputGroup>
+          </Autocomplete> 
         </LoadScript>
-
-        </div>
-      )
-    }
+      </div>
+    )
+  }
 }
-
 
 const mapStateToProps = state => {
   return {
